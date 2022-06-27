237884d49112fe01a12df05e009eed74b4e10dc4
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-27 22:27:42 +0200

    schema working

--- create_schema.sql
+++ create_schema.sql
@@ -7,3 +7,27 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+  id           serial PRIMARY KEY,
+  line_1       varchar(255),
+  city         varchar(255),
+  state_name   varchar(255),
+  zipcode      integer
+);
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+  id           serial PRIMARY KEY,
+  name_class   varchar(255) NOT NULL,
+  credits      integer NOT NULL
+);
+
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+  id           serial PRIMARY KEY,
+  student_id   integer,
+  class_id     integer,
+  grade        varchar(255)
+);
